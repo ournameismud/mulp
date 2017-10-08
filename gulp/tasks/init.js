24e26896acb9d7897eb7f26beb75c05803a9fcd4
magicspon <leon@ournameismud.co.uk>
2017-10-08 15:39:36 +0100

    added checks to init task

--- gulp/tasks/init.js
+++ gulp/tasks/init.js
@@ -1,10 +1,16 @@
 const gulp = require('gulp')
 const gutil = require('gulp-util')
 const path = require('path')
+const fs = require('fs')
 const merge = require('merge-stream')
 const { handleErrors } = require('../libs/utils')
 
 function initConfig(cb) {
+	if (fs.existsSync('config')) {
+		console.info('config folder already exists, skipping init:config task')
+		return
+	}
+
 	return gulp
 		.src([
 			'gulp/path.config.json',
@@ -16,6 +22,11 @@ function initConfig(cb) {
 }
 
 function scaffoldProject(cb) {
+	if (fs.existsSync('src')) {
+		console.info('src folder already exists, skipping init:files task')
+		return
+	}
+
 	return gulp
 		.src(['src/**/*', '*.gitkeep'])
 		.pipe(gulp.dest(path.join(process.env.PWD, PATH_CONFIG.src)))
